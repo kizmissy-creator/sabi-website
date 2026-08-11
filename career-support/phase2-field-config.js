6d73c811b05d946ce5f77e055e3ce94a4b6f8de0
kizmissy-creator <304944019+kizmissy-creator@users.noreply.github.com>
2026-08-11 01:10:38 +0100

    Add Current Situation field options

--- career-support/phase2-field-config.js
+++ career-support/phase2-field-config.js
@@ -14,5 +14,17 @@ window.SABI_PHASE2_FIELD_CONFIG = Object.freeze({
     Object.freeze({ value: 'yes', label: 'Yes' }),
     Object.freeze({ value: 'no', label: 'No' }),
     Object.freeze({ value: 'not-sure', label: 'Not sure' })
+  ]),
+  employmentStatuses: Object.freeze([
+    Object.freeze({ value: 'employed', label: 'Employed' }),
+    Object.freeze({ value: 'self-employed', label: 'Self-employed' }),
+    Object.freeze({ value: 'studying', label: 'Studying or training' }),
+    Object.freeze({ value: 'volunteering', label: 'Volunteering' }),
+    Object.freeze({ value: 'caring', label: 'Caring responsibilities' }),
+    Object.freeze({ value: 'looking-for-work', label: 'Looking for work' }),
+    Object.freeze({ value: 'on-a-break', label: 'On a break from work' }),
+    Object.freeze({ value: 'returning-to-work', label: 'Returning to work' }),
+    Object.freeze({ value: 'no-paid-work', label: 'I have not had paid work yet' }),
+    Object.freeze({ value: 'other', label: 'Other' })
   ])
 });
